9ad63a21cb1c0900c47c545885a416b21976a658
David Mills <millsdg@gmail>
2025-01-02 21:50:23 +0000

    Nothing to see here yet

---
+++ Main.java
@@ -0,0 +1,13 @@
+//Description: Build a system to manage books in a library. Users can add books, issue them, return them, or view available books.
+//Key Learning Points:
+//OOP concepts: Classes (e.g., Book, Library), inheritance, and encapsulation.
+//Use of HashMap or ArrayList to store data.
+//Challenge: Add a search functionality to find books by title or author.
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
