5204bb37c9fea0e09c1adf5abc033abe549ff3d7
David Mills <millsdg@gmail>
2025-01-02 21:50:05 +0000

    first commit

---
+++ Member.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Member {
+    private String name;
+    private int membershipNum;
+    private HashMap<String, String> borrowedBooks;
+
+    public Member(String name){
+        this.name = name;
+    }
+
+
+}
