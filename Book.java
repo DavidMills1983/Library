1eb63a09f6ee5b3024164030eec8dd82f9fc38d1
David Mills <millsdg@gmail>
2025-01-02 21:48:37 +0000

    first commit

---
+++ Book.java
@@ -0,0 +1,28 @@
+public class Book extends LibraryItem {
+
+
+    @Override
+    void makeAvailable() {
+
+    }
+
+    @Override
+    void checkIfAvailable() {
+
+    }
+
+    @Override
+    void getName() {
+
+    }
+
+    @Override
+    void getID() {
+
+    }
+
+    @Override
+    void displayDetails() {
+
+    }
+}
