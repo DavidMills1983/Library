da2e05b6f1e86d11ebee2980b02f6c4439a9a350
David Mills <millsdg@gmail>
2025-01-08 17:12:55 +0000

    Changes made to methods

--- CatalogueService.java
+++ CatalogueService.java
@@ -1,23 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CatalogueService {
+    private final Catalogue catalogue;
 
-    void getItem() {
-
-    }
-
-    void addItem() {
+    public CatalogueService(Catalogue catalogue) {
+        this.catalogue = catalogue;
 
     }
 
-    void deleteItem() {
+    public void getItem(LibraryItem item) {
+        item.displayDetails();
+    }
+
+    public void addItem() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the title of item: ");
+        String newTitle = input.next();
+        System.out.println("Enter the name of the author: ");
+        String newAuthor = input.next();
+        String itemID = Integer.toString(getNumberOfItems() + 1) ;
+        LibraryItem newLibraryItem = new Book(newAuthor, newTitle, itemID);
+
+
 
     }
 
-    void searchItem() {
+    public void deleteItem(String id) {
+        List<LibraryItem> items = catalogue.getItems();
+        items.removeIf(item -> item.getID().equals(id));
+        System.out.println("Item with ID: " + id + " removed.");
+    }
+
+    public LibraryItem searchItem(String title) {
+        for (LibraryItem item : catalogue.getItems()) {
+            if (item.getTitle().equalsIgnoreCase(title)) {
+                System.out.println("Title found: " + item.getTitle());
+                item.displayDetails();
+                return item;
+            }
+
+        }
+        System.out.println("Title not found.");
+        return null;
+    }
+
+    public void displayAllItems() {
+        for (LibraryItem item : catalogue.getItems()){
+            System.out.println(item);
+        }
 
     }
 
-    void displayAllItems() {
+    int getNumberOfItems() {
+        return catalogue.getItems().size();
+    }
 
+    private ArrayList<LibraryItem> getAllItems() {
+        return new ArrayList<>(catalogue.getItems());
     }
 
 }
